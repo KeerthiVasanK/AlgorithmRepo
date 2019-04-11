b12f9b78cf365f763fae3e07773758737554a054
Keerthi Vasan K <keerthivasan_kumarasamy@trimble.com>
2019-04-11 09:55:17 +0530

    Trie for Incremental Search
    Search implemented and code cleanup

--- Incremental Word Search with Trie/Word Search/Word Search/Word Search.cpp
+++ Incremental Word Search with Trie/Word Search/Word Search/Word Search.cpp
@@ -35,7 +35,6 @@ void ConstructData()
 	SearchableList.push_back(Item(10,"Warp"));
 }
 
-
 typedef struct Node_Trie
 {
 	std::map<char, Node_Trie*> children;
@@ -72,14 +71,60 @@ void Insert(Trie* node, Item item, int level = 0)
 		node->bEndOfString = true;
 }
 
+std::vector<long> Search(Trie* node, std::string text, int level = 0)
+{
+	int length = text.length();
+
+	if(level == length)
+		return std::vector<long>();
+
+	char currentChar = text.at(level);
+
+	std::map<char, Node_Trie*>::iterator x = node->children.find(currentChar);
+
+	if(x == node->children.end())
+		return std::vector<long>();
+	else
+	{
+		if(level == length - 1)
+			return x->second->items;
+		else
+			return Search(x->second, text, level +1);
+	}
+
+}
+
+void SearchText()
+{
+	char text[100]; 
+	printf("\n Enter text to search:\t");
+	scanf("%s", text);
+
+	std::vector<long> filteredList = Search(pRoot, std::string(text));
+
+	for(int i = 0; i < filteredList.size(); i++)
+	{
+		std::string value = SearchableList.at(filteredList.at(i) - 1).value;
+
+		printf("%s \n", value.c_str());
+	}
+}
+
+void ConstructTrie()
+{
+	ConstructData();
+
+	for(int i = 0; i < SearchableList.size(); i++)
+		Insert(pRoot, SearchableList.at(i));
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	pRoot = new Trie();
 
-	ConstructData();
+	ConstructTrie();
 
-	for(int i = 0; i < 10; i++)
-		Insert(pRoot, SearchableList.at(i), 0);
+	SearchText();
 
 	int x;
 	scanf("%d",&x);
